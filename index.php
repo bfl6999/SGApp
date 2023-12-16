c5724deb1bbd6edee75028993a16817912f222a9
bfl6999 <115789538+bfl6999@users.noreply.github.com>
2023-12-16 21:25:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Customer bfl699 Catalog</title>
+  <title>Customer bfl699 Catalog What the fault?</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
